76e4be9ddaa0a7c45173251ea5e8c82c3812fdf2
Dylan Foster <dylanfoster935@gmail.com>
2025-08-26 21:01:02 +0100

    Added: Biscuit Manager
    Added a biscuit manager script that uses the classes and allows users to unlock biscuits. I will add functions to set the current biscuit and drink, which can be called in UI when the user selects the biscuit or drink.

---
+++ Assets/Scripts/BiscuitManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BiscuitManager : MonoBehaviour
+{
+    public Biscuit[] biscuits;
+    public Drink[] drinks;
+
+    public Biscuit currentBiscuit;
+    public Drink currentDrink;
+
+    public int crumbs = 0;
+
+    void Start()
+    {
+        biscuits[0].unlocked = true;
+        drinks[0].unlocked = true;
+        currentBiscuit = biscuits[0];
+        currentDrink = drinks[0];
+    }
+
+    public bool UnlockBiscuit(int i)
+    {
+        Biscuit b = biscuits[i];
+        if (!b.unlocked && crumbs >= b.price)
+        {
+            crumbs -= b.price;
+            b.unlocked = true;
+            currentBiscuit = b;
+            return true;
+        }
+        return false;
+    }
+
+    public bool UnlockDrink(int i)
+    {
+        Drink d = drinks[i];
+        if (!d.unlocked && crumbs >= d.price)
+        {
+            crumbs -= d.price;
+            d.unlocked = true;
+            currentDrink = d;
+            return true;
+        }
+        return false;
+    }
+}
