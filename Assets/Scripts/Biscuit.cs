1b32fb288925f59cadd8e29c76df88bdc74002d3
Dylan Foster <dylanfoster935@gmail.com>
2025-08-26 20:50:27 +0100

    Added: Unity Project, Biscuit and Drink script classes, TMPro
    Added the initial Unity project. I made 2 scripts that have the biscuit and drink class contained. I also imported TMPro just because we will definetely use this in the project.

---
+++ Assets/Scripts/Biscuit.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Biscuit
+{
+    public string name;
+    public float scoreMultiplier = 1f;
+    public float integrityMultiplier = 1f;
+    public int price = 0;
+    public bool unlocked = false;
+}
---
+++ Assets/Scripts/Drink.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drink
+{
+    public string name;
+    public float scoreMultiplier = 1f;
+    public float sogginess = 1f;
+    public int price = 0;
+    public bool unlocked = false;
+}
